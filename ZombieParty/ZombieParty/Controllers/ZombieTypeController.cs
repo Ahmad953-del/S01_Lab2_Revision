cc052c369adf0a9109d84067a0dc130f837f017f
Amadou Sow <2486227@cegepmontpetit.ca>
2026-08-31 17:30:29 -0400

    FCT ZombieType Create et Validation
    Ajout de la validation côté client et côté serveur pour ZombieType.

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -21,5 +21,17 @@ namespace ZombieParty.Controllers
             return View();
         }
 
+        //POST
+        [HttpPost]
+        public IActionResult Create(Models.ZombieType zombieType)
+        {
+            if (ModelState.IsValid)
+            {
+                // Ajouter à la BD
+            }
+
+            return this.View(zombieType);
+        }
+
     }
 }
